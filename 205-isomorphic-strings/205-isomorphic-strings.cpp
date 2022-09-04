bbdaa2318096463db5e575866a0287926de0183f
Vibhanshu Pandey <72182909+ivibhu05@users.noreply.github.com>
2022-09-04 15:26:37 +0530

    Time: 20 ms (15.30%), Space: 7.1 MB (46.45%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char,char> mp1;
+        unordered_map<char,char> mp2;
+        for(int i=0;i<s.size();i++)
+        {
+            char c1=s[i];
+            char c2=t[i];
+            if(mp1[c1])
+            {
+                if(mp1[c1]!=c2) return false;
+            }
+            else if(mp2[c2])
+            {
+                if(mp2[c2]!=c1) return false;
+            }
+            else
+            {
+                mp1[c1]=c2;
+                mp2[c2]=c1;
+            }
+        }
+        return true;
+    }
+};
